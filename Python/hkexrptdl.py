90e3f9247ed461efd37109fcad5f10566e36547f
Francis Wu <francisww@msn.com>
2021-05-01 13:30:33 +0800

    extract dates from futures report

--- Python/hkexrptdl.py
+++ Python/hkexrptdl.py
@@ -1,6 +1,7 @@
 import re
 import urllib.request
 from datetime import datetime
+
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -26,7 +27,19 @@ def loadHKExOptions(dt):
         html = f.read().decode('utf-8')
     print (len(html))
     separator = ', '
+
+    #Extract Header Dates 
+    ReportDateStrMatches = re.search('   Business Day([\\s\\S]*?)After-Hours Trading Session', html, re.MULTILINE )
+    print(ReportDateStrMatches.group())
+    matchdates = tuple(re.findall ('(\\d\\d (JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC) \\d\\d\\d\\d)', ReportDateStrMatches.group()))
+    format_str="%d %b %Y"
+    if (len(matchdates)==2):
+        ndt = datetime.strptime(matchdates[0][0], format_str)
+        dt = datetime.strptime(matchdates[1][0], format_str)
+        print(ndt,dt)
+    
     matches = re.finditer('MONTH     PRICE([\\s\\S]*?)TOTAL', html)
+    matches=[]
     for k in matches:
         match_txt = k.group(0)
         rows = match_txt.split('\n')
@@ -35,5 +48,5 @@ def loadHKExOptions(dt):
             if len(cell)==22:
                 print (separator.join(cell))
 
-testDBConnection()
-loadHKExOptions(datetime(2021, 4, 27))
+#testDBConnection()
+loadHKExOptions(datetime(2021, 4, 7))
